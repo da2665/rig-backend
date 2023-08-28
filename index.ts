e4e9123171680e0a538828be9c0c91c9f86247b8
Dylan Armstrong <dylanarmstrong@swipetech.com.au>
2023-08-29 03:31:14 +1000

    Fixing issues with SQL attachment coloumn values being null

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import AWS from "aws-sdk";
 import dotenv from "dotenv";
-import { Server } from "socket.io";
+import { Server, Socket } from "socket.io";
 import * as chat from "./types/chat";
 import { sendMessage } from "./methods/sendMessage";
 import { getMessages } from "./methods/getMessages";
@@ -11,44 +11,32 @@ dotenv.config();
 
 const io = new Server({
   cors: {
-    origin: ["http://localhost:3000"],
-    methods: ["GET", "POST"]
+    origin: "http://localhost:3000",
   },
 });
 
-io.on("connection", (socket: any) => {
+io.on("connection", (socket: Socket) => {
   socket.on("Get Messages", async () => {
     try {
-      const messages = await getMessages();
-      socket.emit("Messages", JSON.parse(messages as string));
-      socket.emit("New Message", JSON.parse(messages as string));
+      const messages = JSON.parse((await getMessages()) as unknown as string);
+      socket.emit("Messages", messages);
     } catch (error) {
       console.error(error);
+      throw error;
     }
   });
 
   socket.on("Send Message", async (message: chat.Message) => {
-    try {
-      const messages = JSON.parse(
-        (await getMessages()) as string
-      ) as chat.Message[];
-
-      const request: chat.Message = {
-        id: await generateId(messages.length),
-        sender: message.sender,
-        receiver: message.receiver,
-        contents: message.contents,
-        attachments: message.attachments,
-      };
-
-      await sendMessage(request);
-    } catch (error) {
-      console.error(error);
-    }
-  });
-
-  socket.on("disconnect", () => {
-    socket.removeAllListeners();
+    const messages = JSON.parse((await getMessages()) as unknown as string);
+    
+    const request: chat.Message = {
+      id: await generateId(messages.length),
+      sender: message.sender,
+      receiver: message.receiver,
+      contents: message.contents,
+      attachments: message.attachments,
+    };
+    socket.emit("New Message", await sendMessage(request));
   });
 });
 
--- methods/sendMessage.ts
+++ methods/sendMessage.ts
@@ -2,33 +2,23 @@ import * as chat from "../types/chat";
 import { connect } from "../db/db";
 import { encryptMessage } from "./encryptMessage";
 
-async function getPoolQueryData(message: chat.Message) {
-  const db = await connect();
-  const encrypted_message = await encryptMessage(message);
-  return db.getConnection((err, connection) => {
-    try {
-      connection.query(
-        `INSERT INTO messages (id, sender, receiver, contents, attachments) VALUES(?,?,?,?,?)`,
-        [
-          encrypted_message.id,
-          encrypted_message.sender,
-          encrypted_message.receiver,
-          encrypted_message.contents,
-          encrypted_message.attachments,
-        ],
-        (err: any, res: any) => {
-          return res;
-        }
-      );
-    } catch (error) {
-      console.error(error);
-      throw error;
-    } finally {
-      connection.release();
-    }
-  });
-}
-
 export async function sendMessage(message: chat.Message) {
-  return await getPoolQueryData(message);
+  const connection = await connect();
+  return new Promise((resolve, reject) => {
+    connection.query(
+      `INSERT INTO messages (id, sender, receiver, contents, attachments) VALUES ?`,
+      [
+        message.id,
+        message.sender,
+        message.receiver,
+        message.contents,
+        message.attachments,
+      ],
+      (err, res) => {
+        if (err) reject(err);
+        else resolve(res);
+        connection.end();
+      }
+    );
+  });
 }
